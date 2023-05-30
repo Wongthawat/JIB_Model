45effb3626ca0f8e2c6dc155ba1982aa87184fc8
Wongthawat <89896981+Wongthawat@users.noreply.github.com>
2023-05-30 09:51:41 +0700

    update

--- src/pages/BranchContact/BranchContact.jsx
+++ src/pages/BranchContact/BranchContact.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
 
 const BranchContact = () => {
-  return <div>Map</div>;
+  const defaultProps = {
+    center: {
+      lat: 13.942906290026817,
+      lng: 100.62309892362283,
+    },
+    zoom: 11,
+  };
+
+  return (
+    <div className="row">
+      <div className="col-lg-4"></div>
+      <div className="col-lg-8">
+        <div style={{ height: "100vh", width: "100%" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: "" }}
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+          ></GoogleMapReact>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BranchContact;
